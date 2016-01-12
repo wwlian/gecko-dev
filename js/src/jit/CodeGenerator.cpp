8dfb13042367b1d8b7cab99e76cc687a66210852
Wilson Lian <wwlian@gmail.com>
2016-01-12 13:27:58 -0800

    Removing commented-out JIT code byte emission code that wrote directly to stdout rather than using JitSpew.

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -8328,14 +8328,6 @@ CodeGenerator::link(JSContext* cx, CompilerConstraintList* constraints)
     }
     buf[code->instructionsSize() * 4] = '\0';
     JitSpew(JitSpew_CodeBytes, "Raw Ion bytes (%d) for %s:%d:@%p:%s", code->instructionsSize(), script->filename(), script->lineno(), code->raw(), buf);
-    /*
-    size_t finalSize = code->instructionsSize() * 4 + 1 + 200;
-    char *finalBuf = js_pod_malloc<char>(finalSize);
-    snprintf(finalBuf, finalSize, "\nRaw Ion bytes (%d) for %s:%d:@%p:%s\n", code->instructionsSize(), script->filename(), script->lineno(), code->raw(), buf);
-    write(1, finalBuf, strlen(finalBuf));  // Write directly to hopefully avoid interleaving.
-    fflush(stdout);
-    js_free(finalBuf);
-    */
     js_free(buf);
 
     ionScript->setInvalidationEpilogueDataOffset(invalidateEpilogueData_.offset());
