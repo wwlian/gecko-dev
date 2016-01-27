e58ad07030d8e1d4f521470fc7434b7c807cf3ad
Wilson Lian <wwlian@gmail.com>
2016-01-26 20:32:36 -0800

    Implementing ARM constant blinding for bitwise ops in IonMonkey.

--- js/src/jit/arm/CodeGenerator-arm.cpp
+++ js/src/jit/arm/CodeGenerator-arm.cpp
@@ -827,6 +827,47 @@ CodeGeneratorARM::visitBitNotI(LBitNotI* ins)
     masm.ma_mvn(ToRegister(input), ToRegister(dest));
 }
 
+#ifdef CONSTANT_BLINDING
+void
+CodeGeneratorARM::visitBitOpI(LBitOpI* ins)
+{
+    const LAllocation* lhs = ins->getOperand(0);
+    const LAllocation* rhs = ins->getOperand(1);
+    const LDefinition* dest = ins->getDef(0);
+    // All of these bitops should be either imm32's, or integer registers.
+    switch (ins->bitop()) {
+      case JSOP_BITOR:
+        if (rhs->isConstant()) {
+        	uint32_t secret = static_cast<uint32_t>(asMasm().blindingValue());
+            masm.ma_orr(Imm32(ToInt32(rhs) & secret), ToRegister(lhs), ToRegister(dest));
+            masm.ma_orr(Imm32(ToInt32(rhs) & ~secret), ToRegister(dest), ToRegister(dest));
+        } else {
+            masm.ma_orr(ToRegister(rhs), ToRegister(lhs), ToRegister(dest));
+        }
+        break;
+      case JSOP_BITXOR:
+        if (rhs->isConstant()) {
+        	uint32_t secret = static_cast<uint32_t>(asMasm().blindingValue());
+            masm.ma_eor(Imm32(ToInt32(rhs) ^ secret), ToRegister(lhs), ToRegister(dest));
+            masm.ma_eor(secret), ToRegister(dest), ToRegister(dest));
+        } else {
+            masm.ma_eor(ToRegister(rhs), ToRegister(lhs), ToRegister(dest));
+        }
+        break;
+      case JSOP_BITAND:
+        if (rhs->isConstant()) {
+        	uint32_t secret = static_cast<uint32_t>(asMasm().blindingValue());
+            masm.ma_and(Imm32(ToInt32(rhs) | secret), ToRegister(lhs), ToRegister(dest));
+            masm.ma_and(Imm32(ToInt32(rhs) | ~secret), ToRegister(dest), ToRegister(dest));
+        } else {
+            masm.ma_and(ToRegister(rhs), ToRegister(lhs), ToRegister(dest));
+        }
+        break;
+      default:
+        MOZ_CRASH("unexpected binary opcode");
+    }
+}
+#else
 void
 CodeGeneratorARM::visitBitOpI(LBitOpI* ins)
 {
@@ -857,6 +898,7 @@ CodeGeneratorARM::visitBitOpI(LBitOpI* ins)
         MOZ_CRASH("unexpected binary opcode");
     }
 }
+#endif
 
 void
 CodeGeneratorARM::visitShiftI(LShiftI* ins)
