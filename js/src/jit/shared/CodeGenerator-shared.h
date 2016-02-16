053d490cace8d74e32d7682e66b5d4be8408a3a1
Wilson Lian <wwlian@gmail.com>
2016-02-16 12:36:55 -0800

    Removing unneeded ARM-specific IonMonkey constant blinding now that MIR-level blinding is in place.

--- js/src/jit/shared/CodeGenerator-shared.h
+++ js/src/jit/shared/CodeGenerator-shared.h
@@ -72,9 +72,6 @@ class CodeGeneratorShared : public LElementVisitor
     MacroAssembler& masm;
 
   protected:
-#ifdef CONSTANT_BLINDING
-    RNG rng;
-#endif
     MIRGenerator* gen;
     LIRGraph& graph;
     LBlock* current;
